5172051021621f6febd4210efae69a180ba98a0e
jennyf19 <ferriesj@gmail.com>
2017-10-26 12:30:15 -0700

    adding more

---
+++ BinaryTreeMerge/Node.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace BinaryTreeMerge
+{
+    public class Node
+    {
+        int data;
+        public Node left;
+        public Node right;
+
+        public Node(int data, Node left, Node right)
+        {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+
+        // Helper method that allocates a new node
+        public static Node NewNode(int data)
+        {
+            return new Node(data, null, null);
+        }
+
+        // Given a binary tree, print its node inorder
+        public static void InOrderSearch(Node node)
+        {
+            if (node == null)
+                return;
+
+            // recurse left side
+            InOrderSearch(node.left);
+
+            // print out the data of the node
+            Console.Write("{0}", node.data);
+
+            // recurse right side
+            InOrderSearch(node.right);
+        }
+
+        // Merge two binary trees
+        public static Node MergeBinaryTrees(Node t1, Node t2)
+        {
+            if(t1 == null)
+            {
+                return t2;
+            }
+            if(t2 == null)
+            {
+                return t1;
+            }
+            t1.data += t2.data;
+            t1.left = MergeBinaryTrees(t1.left, t2.left);
+            t1.right = MergeBinaryTrees(t1.right, t2.right);
+            return t1;
+        }
+    }
+}
---
+++ BinaryTreeMerge/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BinaryTreeMerge
+{
+    class Program
+    {
+        // Merge binary trees
+        static void Main(string[] args)
+        {
+            Node root1 = Node.NewNode(1);
+            root1.left = Node.NewNode(2);
+            root1.right = Node.NewNode(3);
+            root1.left.left = Node.NewNode(4);
+            root1.left.right = Node.NewNode(5);
+            root1.right = Node.NewNode(6);
+
+            Node root2 = Node.NewNode(4);
+            root2.left = Node.NewNode(1);
+            root2.right = Node.NewNode(7);
+            root2.left.left = Node.NewNode(3);
+            root2.right.left = Node.NewNode(2);
+            root2.right.right = Node.NewNode(6);
+
+            Node root3 = Node.MergeBinaryTrees(root1, root2);
+            Console.WriteLine("The merged binary tree is:\n");
+            Node.InOrderSearch(root3);
+
+            Console.ReadLine();
+        }
+    }
+}
